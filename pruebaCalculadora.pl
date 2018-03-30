bded7f63adedd66a428408aa4adf76459032208d
nilsarmiento <nilsarmiento@hotmail.com>
2018-03-30 18:26:15 -0300

    terminada la calculadora con json

--- pruebaCalculadora.pl
+++ pruebaCalculadora.pl
@@ -6,7 +6,7 @@ use lib "./modules";
 use Calculadora;
 
 
-my @salida=Calculadora::GetRadiacion(-24.9124,-65.3961,"menero");
+my @salida=Calculadora::GetRadiacion(-24.9124,-65.3961,"denero");
 
 
 #($mes, $beta, $latitud, $H)
